051bdc88e74b02b3ee66596417584e8cf73f5afe
saimun5656 <shaounlosted@gmail.com>
2023-02-14 01:31:25 +0600

    balance updating addded

--- js/deposite.js
+++ js/deposite.js
@@ -4,4 +4,9 @@ const prevoiusDeposite = document.getElementById('current-deposite').innerText
 const result = parseInt(newDeposite)+parseInt(prevoiusDeposite) 
 document.getElementById('current-deposite').innerText=result
 document.getElementById('deposite-amount').value = ''
+const balancElement =document.getElementById('Balance')
+const balanceTotal =parseFloat(balancElement.innerText)
+console.log(balanceTotal);
+const totalDeposite =parseFloat(document.getElementById('current-deposite').innerText)
+balancElement.innerText= balanceTotal+parseFloat(newDeposite)
 })
