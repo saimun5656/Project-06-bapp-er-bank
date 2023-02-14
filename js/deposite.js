3a7578cd3ea1f2d967dd87e8be0c302631c5026f
saimun5656 <shaounlosted@gmail.com>
2023-02-14 21:49:03 +0600

    deposite and withdrow final

--- js/deposite.js
+++ js/deposite.js
@@ -1,12 +1,28 @@
 document.getElementById('deposite-btn').addEventListener('click',function(){
 const newDeposite =document.getElementById('deposite-amount').value
 const prevoiusDeposite = document.getElementById('current-deposite').innerText
-const result = parseInt(newDeposite)+parseInt(prevoiusDeposite) 
+const result = parseInt(newDeposite)+parseInt(prevoiusDeposite);
 document.getElementById('current-deposite').innerText=result
 document.getElementById('deposite-amount').value = ''
 const balancElement =document.getElementById('Balance')
-const balanceTotal =parseFloat(balancElement.innerText)
-console.log(balanceTotal);
-const totalDeposite =parseFloat(document.getElementById('current-deposite').innerText)
+const balanceTotal =parseFloat(balancElement.innerText);
 balancElement.innerText= balanceTotal+parseFloat(newDeposite)
+})
+
+
+document.getElementById('withdrow-btn').addEventListener('click',function(){
+const withdrowFIeld =document.getElementById('withdrow-amount');
+const withdrowAmount =parseFloat(withdrowFIeld.value);
+const balancElement =document.getElementById('Balance');
+const balance = parseFloat(balancElement.innerText);
+const currentwithdrow =document.getElementById('current-withdrow');
+const currentwithdrowAmount =parseFloat(currentwithdrow.innerText);
+withdrowFIeld.value=''
+if(withdrowAmount>balance){
+    alert('not enough balance')
+    return;
+}
+balancElement.innerText =balance-withdrowAmount
+currentwithdrow.innerText=currentwithdrowAmount+withdrowAmount
+
 })
